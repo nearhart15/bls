550972ea379001c63a81513135725053816a6bc3
nearhart15 <42108586+nearhart15@users.noreply.github.com>
2026-09-16 20:05:09 -0600

    Load responsive player list styles

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@
 import 'bootswatch/dist/lumen/bootstrap.min.css';
 import './sass/bls.scss';
 import './sass/matchup-alignment.scss';
+import './sass/player-list-responsive.scss';
 
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
